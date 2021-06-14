69a8a7712b4a123c8db8a9b6fdaea44b50720e83
benpercifield <30870091+benpercifield@users.noreply.github.com>
2021-06-14 10:45:15 -0400

    Update MeetingController.ino

--- Arduino/MeetingController/MeetingController.ino
+++ Arduino/MeetingController/MeetingController.ino
@@ -1,10 +1,11 @@
 /*
-  MeetingController
-  created 01 Feb 2021
+  MeetingController - No Toggle
+  created 25 Feb 2021
   by Hugh Elliott (hugh@thinkingbox.com)
-
   Original example code is in the public domain.
   http://www.arduino.cc/en/Tutorial/KeyboardMessage
+  Thanks Hugh - Ben
+  I updated the camera but to switch btw desktops
 */
 
 #include "Keyboard.h"
@@ -12,22 +13,15 @@
 const int windowPin = 7;
 const int mutePin = 6;
 const int cameraPin = 5; 
-const int zoomIndPin = 11;
-const int meetIndPin = 10; 
-const int zoomPin = 9;
-//const int meetPin = 8;
 
 int windowState = 0; 
-int muteState = 0; 
+int muteState = 0;
 int cameraState = 0; 
 int prevWinState = 1;
 int prevMuteState = 1;
 int prevCamState = 1;
-int zoomState = 0; 
-int meetState = 0; 
-int prevZoomState = 1;
-//int prevMeetState = 1;
-
+int desktopState = 0;
+int prevDesktopState = 1;
 boolean windowToggle = false;
 boolean muteToggle = false;
 boolean cameraToggle = false;
@@ -37,42 +31,25 @@ void setup() {
   pinMode(windowPin, INPUT);
   pinMode(mutePin, INPUT);
   pinMode(cameraPin, INPUT);
-  pinMode(zoomIndPin, OUTPUT);
-  pinMode(meetIndPin, OUTPUT);
   // initialize control over the keyboard:
   digitalWrite(windowPin,LOW);
   digitalWrite(mutePin,LOW);
   digitalWrite(cameraPin,LOW);
-  //digitalWrite(zoomPin,LOW);
-  //digitalWrite(meetPin,LOW);
-  //Keyboard.begin();
-  Serial.begin(9600);
-    digitalWrite(zoomIndPin,digitalRead(zoomPin));
-    //digitalWrite(meetIndPin,digitalRead(meetPin));
+  Keyboard.begin();
+  //Serial.begin(9600);
 }
 void loop() {
   windowState = digitalRead(windowPin);
   muteState = digitalRead(mutePin);
   cameraState = digitalRead(cameraPin);
-  zoomState = digitalRead(zoomPin);
-  //meetState = digitalRead(meetPin);
-  
-  digitalWrite(zoomIndPin,zoomState);
-  digitalWrite(meetIndPin,!zoomState);
   if(windowState == HIGH){
     windowToggle = true;
   }else{
     if(windowToggle == true){
       Keyboard.press(KEY_LEFT_GUI);
-      delay(100);
-      Keyboard.press('w');
-      delay(100);
-      if(zoomState == HIGH){
-      Keyboard.releaseAll(); 
-        delay(1000);
-        Keyboard.press(KEY_RETURN);
-        delay(100);
-      }
+      delay(20);
+      Keyboard.press('d');
+      delay(20);
       Keyboard.releaseAll(); 
       windowToggle = false;
     }
@@ -83,14 +60,7 @@ void loop() {
     if(muteToggle == true){
       Keyboard.press(KEY_LEFT_GUI);
       delay(100);
-      if(zoomState == HIGH){
-        Keyboard.press(KEY_LEFT_SHIFT);
-        delay(100);
-        Keyboard.press('a');
-      }else{
-      Keyboard.press('d');
-      }
-      delay(100);
+      Keyboard.press('w');
       Keyboard.releaseAll();
       muteToggle = false;
     }
@@ -99,19 +69,24 @@ void loop() {
     cameraToggle = true;
   }else{
     if(cameraToggle == true){
-      Keyboard.press(KEY_LEFT_GUI);
-      delay(100);
-      if(zoomState == HIGH){
-        Keyboard.press(KEY_LEFT_SHIFT);
-        delay(100);
-        Keyboard.press('v');
-      }else{
-      Keyboard.press('e');
-      }
-      delay(100);
+      if(desktopState == 0)
+      {
+      Keyboard.press(KEY_RIGHT_CTRL);
+      delay(20);
+      Keyboard.press(KEY_RIGHT_ARROW);
       Keyboard.releaseAll();
+      desktopState = 1;
       cameraToggle = false;
+      }
+      else
+      {
+      Keyboard.press(KEY_RIGHT_CTRL);
+      delay(20);
+      Keyboard.press(KEY_LEFT_ARROW);
+      Keyboard.releaseAll();
+      desktopState = 0;
+      cameraToggle = false; 
+      }
+      }
     }
   }
-delay(250);
-}
